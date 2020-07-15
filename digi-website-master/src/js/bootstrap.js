ad289286e7396dff1309c222e61d3db77fd3e3f3
rajan-mahida <rajan.mahida@digipli.com>
2020-07-15 13:31:13 -0400

    update0

--- digi-website-master/src/js/bootstrap.js
+++
@@ -1,6 +0,0 @@
-try {
-    window._ = require('lodash');
-    window.$ = window.jQuery = require('jquery');
-    window.Popper = require('popper.js').default;
-    require('bootstrap');
-} catch (e) {}
